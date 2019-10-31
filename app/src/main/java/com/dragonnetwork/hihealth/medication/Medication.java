dca2df54139cd76306f6548958663f6782152238
spark288 <sungjinpark503@gmail.com>
2019-10-30 21:47:58 -0500

    Medication Class

---
+++ app/src/main/java/com/dragonnetwork/hihealth/medication/Medication.java
@@ -0,0 +1,48 @@
+package com.dragonnetwork.hihealth.medication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dragonnetwork.hihealth.MainActivity;
+
+import java.util.ArrayList;
+
+public class Medication extends AppCompatActivity {
+    EditText et;
+    Button bt;
+    ListView lv;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et = (EditText) findViewById(R.id.editText);
+        bt = (Button) findViewById(R.id.button_addData);
+        lv = (ListView) findViewById(R.id.listView_lv);
+
+        arrayList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(Medication.this, android.R.layout.med_list_item, arrayList);
+        lv.setAdapter(adapter);
+
+        onBtnClick();
+    }
+
+    public void onBtnClick() {
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = et.getText().toString();
+                arrayList.add(result);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
