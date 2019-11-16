e3e2590760c566aab7a41190c5ad4796697d7cfd
Jake <jparker22@wisc.edu>
2019-11-15 21:08:31 -0600

    User profile displays correct name and email

--- app/src/main/java/com/dragonnetwork/hihealth/user/UserProfile.java
+++ app/src/main/java/com/dragonnetwork/hihealth/user/UserProfile.java
@@ -6,35 +6,59 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dragonnetwork.hihealth.MainActivity;
 import com.dragonnetwork.hihealth.R;
 import com.dragonnetwork.hihealth.data.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 public class UserProfile extends AppCompatActivity {
 
     private static TextView view_username;
     private static TextView view_email;
     private static TextView view_phone;
+
+    private static FirebaseFirestore db;
+    private static CollectionReference userDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile_activity);
-        if (savedInstanceState == null) {
-            /*getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, UserProfileFragment.newInstance())
-                    .commitNow();*/
-        }
-        //ActionBar actionBar = getActionBar();
-        //actionBar.setDisplayHomeAsUpEnabled(true);
 
+        db = FirebaseFirestore.getInstance();
+        userDB = db.collection("Users");
         view_username = findViewById(R.id.username);
-        //view_username.setText("");
         view_email = findViewById(R.id.email);
-        //view_username.setText(User.Email);
-        view_phone = findViewById(R.id.phone);
-        //view_username.setText(User.Phone);
+
+        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        userDB.document(userID).get().addOnCompleteListener(
+                new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        Map<String, Object> result;
+                        result = task.getResult().getData();
+                        final String user_name = result.get("Name").toString();
+                        final String email = result.get("Email").toString();
+                        view_username.setText(user_name);
+                        view_email.setText(email);
+                    }
+                }
+        );
+
+//        view_phone = findViewById(R.id.phone);
+
+
         final Button edit_button = findViewById(R.id.profile_edit_button);
         edit_button.setOnClickListener(new View.OnClickListener() {
             @Override
