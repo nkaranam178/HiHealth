b546a4260dd082a9837822dcdadb60095a779ff2
Jake <jparker22@wisc.edu>
2019-11-15 22:22:56 -0600

    can change email

--- app/src/main/java/com/dragonnetwork/hihealth/user/UserProfile.java
+++ app/src/main/java/com/dragonnetwork/hihealth/user/UserProfile.java
@@ -1,12 +1,16 @@
 package com.dragonnetwork.hihealth.user;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dragonnetwork.hihealth.MainActivity;
@@ -17,19 +21,30 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Transaction;
 
 import java.util.Map;
 
 public class UserProfile extends AppCompatActivity {
 
     private static TextView view_username;
-    private static TextView view_email;
-    private static TextView view_phone;
+    private static EditText et_email;
+    private static EditText view_phone;
+    private static EditText view_docname;
+    private static EditText view_docemail;
+    private static EditText view_docphone;
+
+    private static String userFullName;
+    private static String userEmail;
+    private static String userPhone;
 
     private static FirebaseFirestore db;
     private static CollectionReference userDB;
+    private static DocumentReference userDoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,43 +54,81 @@ public class UserProfile extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
         userDB = db.collection("Users");
         view_username = findViewById(R.id.username);
-        view_email = findViewById(R.id.email);
-
+        et_email = findViewById(R.id.email);
         String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        userDB.document(userID).get().addOnCompleteListener(
+        userDoc = userDB.document(userID);
+
+        userDoc.get().addOnCompleteListener(
                 new OnCompleteListener<DocumentSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         Map<String, Object> result;
                         result = task.getResult().getData();
                         final String user_name = result.get("Name").toString();
+                        userFullName = user_name;
                         final String email = result.get("Email").toString();
+                        userEmail = email;
                         view_username.setText(user_name);
-                        view_email.setText(email);
+                        et_email.setText(email);
                     }
                 }
         );
 
 //        view_phone = findViewById(R.id.phone);
 
-
         final Button edit_button = findViewById(R.id.profile_edit_button);
         edit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                if (onEditButtonPressed()) {
+                    // successful
+                } else {
+                    // failure occurred - handle it or something?...
+                }
             }
         });
     }
-    /*public void signout(){
-        User.signout();
-    }*/
-    /*public static void updateProfile(){
-        view_username.setText(User.Username);
-        view_email.setText(User.Email);
-        view_phone.setText(User.Phone);
-    }*/
+
+    private boolean onEditButtonPressed() {
+        AlertDialog.Builder builder;
+
+        // only make changes to database if user edited the fields. Otherwise ignore button press
+        // make sure to check if other edit texts are altered when they are implemented
+//        if (!userFullName.equals(view_username.getText().toString())) {
+            // update the data in firebase
+            final String email = et_email.getText().toString();
+            builder = new AlertDialog.Builder(this).
+            setTitle("Confirm Profile Edit").
+            setMessage("Save your changes?").
+            setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    db.runTransaction(new Transaction.Function<Void>() {
+                        @Override
+                        public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                            transaction.update(userDoc, "Email", email);
+                            return null;
+                        }
+                    });
+                    Toast.makeText(
+                            UserProfile.this,
+                            "Changes Saved.",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            }).
+            setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(
+                            UserProfile.this,
+                            "Cancelled changes.",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            });
+            builder.show();
+//        }
+        return true;  // TODO check for failure, if failed return false
+    }
 }
