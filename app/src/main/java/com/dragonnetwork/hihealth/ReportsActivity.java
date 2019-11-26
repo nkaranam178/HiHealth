f26209e45932eb81d377fb19d909c22156103a28
nkaranam178 <30273570+nkaranam178@users.noreply.github.com>
2019-11-26 13:29:12 -0600

    Update ReportsActivity.java
    Navbar displays user name

--- app/src/main/java/com/dragonnetwork/hihealth/ReportsActivity.java
+++ app/src/main/java/com/dragonnetwork/hihealth/ReportsActivity.java
@@ -1,41 +1,212 @@
 package com.dragonnetwork.hihealth;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 import android.net.Uri;
 import android.telephony.SmsManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
 
-public class ReportsActivity extends AppCompatActivity {
+import com.dragonnetwork.hihealth.data.User;
+import com.dragonnetwork.hihealth.medication.MedicationActivity;
+import com.dragonnetwork.hihealth.user.UserProfile;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class ReportsActivity extends MainActivity implements NavigationView.OnNavigationItemSelectedListener {
     String messageToSend = "Emergency Text";
-    String number = "6086220769";
-    SmsManager sm = SmsManager.getDefault();
+    String number = "6086220769"; // temporary
+    TextView textView;
+    Button button;
+
+    private Toolbar toolbar;
+    protected DrawerLayout drawer;
+    protected ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
 
-        findViewById(R.id.callPhysician).setOnClickListener(new View.OnClickListener() {
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.reports_layout);
+        onCreateDrawer();
+
+        View header = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView)header.findViewById(R.id.username);
+        navUsername.setText(User.getName());
+        header.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialContactPhone("6086220769");
+                Intent intent;
+                intent = new Intent(ReportsActivity.this, UserProfile.class);
+                startActivity(intent);
             }
         });
 
-//        findViewById(R.id.textPhysician).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                sm.sendTextMessage(number, null, messageToSend, null,null);
-//            }
-//        });
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(ReportsActivity.this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        findViewById(R.id.callPhysician).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("+16086220769");
+            }
+        });
+
+        button = findViewById(R.id.textPhysician);
+        findViewById(R.id.textPhysician).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMS(number,messageToSend);
+            }
+        });
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("PLAYGROUND", "Permission is not granted, requesting");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 123);
+            button.setEnabled(false);
+        } else {
+            Log.d("PLAYGROUND", "Permission is granted");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 123) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("PLAYGROUND", "Permission has been granted");
+                textView.setText("You can send SMS!");
+                button.setEnabled(true);
+            } else {
+                Log.d("PLAYGROUND", "Permission has been denied or request cancelled");
+                textView.setText("You can not send SMS!");
+                button.setEnabled(false);
+            }
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Intent intent;
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_medications:
+                intent = new Intent(this, MedicationActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            case R.id.nav_reminders:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            case R.id.nav_calendar:
+                intent = new Intent(this, CalendarActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            case R.id.nav_reports:
+                drawer.closeDrawer(Gravity.LEFT);
+                break;
+            case R.id.nav_settings:
+                // must implement settings activity
+                break;
+            case R.id.nav_symptoms:
+                // must implement settings activity
+                break;
+        }
+
+        return true;
     }
 
     private void dialContactPhone(final String phoneNumber) {
         startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+    }
+
+    private void sendSMS(String phoneNumber, String message) {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
     }
 
 
