85be09941624ff4e711d3a7c212402e9ebcd9621
nkaranam178 <30273570+nkaranam178@users.noreply.github.com>
2019-11-26 13:23:06 -0600

    Update CalendarActivity.java
    Navbar displays username

--- app/src/main/java/com/dragonnetwork/hihealth/CalendarActivity.java
+++ app/src/main/java/com/dragonnetwork/hihealth/CalendarActivity.java
@@ -1,20 +1,66 @@
 package com.dragonnetwork.hihealth;
 
 import java.util.Calendar;
-import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.CalendarView;
 import android.util.Log;
+import android.widget.TextView;
 
-public class CalendarActivity extends AppCompatActivity {
+import com.dragonnetwork.hihealth.data.User;
+import com.dragonnetwork.hihealth.medication.MedicationActivity;
+import com.dragonnetwork.hihealth.user.UserProfile;
+import com.google.android.material.navigation.NavigationView;
+
+public class CalendarActivity extends MainActivity implements NavigationView.OnNavigationItemSelectedListener {
     private  static final String TAG = "CalendarActivity";
     private CalendarView mCalendarView;
+
+    protected Toolbar toolbar;
+    protected DrawerLayout drawer;
+    protected ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        contentView = R.id.nav_calendar;
         setContentView(R.layout.activity_calendar);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.calendar_layout);
+        onCreateDrawer();
+
+        View header = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView)header.findViewById(R.id.username);
+        navUsername.setText(User.getName());
+        header.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(CalendarActivity.this, UserProfile.class);
+                startActivity(intent);
+            }
+        });
+
+//        navigationView = findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(CalendarActivity.this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         mCalendarView = (CalendarView) findViewById(R.id.calendarView);
         mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -27,6 +73,41 @@ public class CalendarActivity extends AppCompatActivity {
                 addCalendarEvent();
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Intent intent;
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_medications:
+                intent = new Intent(this, MedicationActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            case R.id.nav_reminders:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            case R.id.nav_calendar:
+                drawer.closeDrawer(Gravity.LEFT);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            case R.id.nav_reports:
+                intent = new Intent(this, ReportsActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            case R.id.nav_settings:
+                // must implement settings activity
+                break;
+            case R.id.nav_symptoms:
+                // must implement settings activity
+                break;
+        }
+
+        return true;
     }
 
     public void addCalendarEvent(){
